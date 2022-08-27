ed02e13ba7537bfed9989c18c769c33975229ed7
Ajay KC <meajay64@gmail.com>
2022-08-27 15:47:12 +0900

    Make image properly load

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,17 +1,12 @@
 import Image from "./Image";
 
-function ImageGallery() {
-  return (
-    <div className="sm:w-9/12 mx-auto sm:grid grid-cols-3 gap-4">
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-    </div>
-  );
+function ImageGallery({ images }) {
+  let image = images.map((image) => {
+     return <Image key={image.id} image={image} />;
+  });
+  return <div className="sm:w-9/12 mx-auto sm:grid grid-cols-3 gap-4">
+    {image}
+  </div>;
 }
 
 export default ImageGallery;
